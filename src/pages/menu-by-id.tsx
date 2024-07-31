b951c2db187cb4142990a3782ebfe7723233c075
Jxwgame <109953079+Jxwgame@users.noreply.github.com>
2024-07-31 17:01:04 +0700

    Add files via upload

--- src/pages/menu-by-id.tsx
+++ src/pages/menu-by-id.tsx
@@ -57,7 +57,7 @@ export default function MenuByIdPage() {
         message: "สั่งซื้อเรียบร้อยแล้ว",
         color: "teal",
       });
-      navigate(`/menu`);
+      navigate(`/orders/${response.data.id}`);
     } catch (error) {
       if (error instanceof AxiosError) {
         if (error.response?.status === 422) {
@@ -82,6 +82,7 @@ export default function MenuByIdPage() {
         });
       }
     } finally {
+      navigate(`/menu`);
       setIsProcessing(false);
     }
   };
